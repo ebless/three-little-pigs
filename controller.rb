4d0ef1d2f61d646bccc5ea9c0cf5fe191e5bf625
Ned Bless <ebless@lsoc.org>
2016-04-21 11:12:55 -0500

    Add framework

--- controller.rb
+++ controller.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
 get '/' do
+	@question = "You are a pig who is building your first house. What are you going to build it out of?"
+	
+	@choice_1 = "straw"
+	@choice_2 = "wood"
+	@choice_3 = "brick"
+	
 	erb :index
+end
+
+post '/first_choice' do
+
 end
